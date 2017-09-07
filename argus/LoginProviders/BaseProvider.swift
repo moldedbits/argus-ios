0ac4d4f787361735ae33ffc9d9770459a7501227
Saurabh Gupta <saurabh@moldedbits.com>
2017-09-07 16:59:01 +0530

    initial project setup

---
+++ argus/LoginProviders/BaseProvider.swift
@@ -0,0 +1,13 @@
+//
+//  BaseProvider.swift
+//  argus
+//
+//  Created by Saurabh Gupta on 07/09/17.
+//  Copyright © 2017 moldedbits. All rights reserved.
+//
+
+import Foundation
+
+protocol BaseProvider {
+    
+}
---
+++ argus/LoginProviders/FacebookLoginProvider.swift
@@ -0,0 +1,14 @@
+//
+//  FacebookLoginProvider.swift
+//  argus
+//
+//  Created by Saurabh Gupta on 07/09/17.
+//  Copyright © 2017 moldedbits. All rights reserved.
+//
+
+import Foundation
+//import FacebookCore
+
+class FacebookLoginProvider: BaseProvider {
+    
+}
