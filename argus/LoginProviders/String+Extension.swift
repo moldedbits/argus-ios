54af8e199d123d7a9fd1f7029f5aae3fd9a4e2a0
Amit Kumar Swami <amitiitm2009@gmail.com>
2017-09-08 17:31:58 +0530

    Add String helper methods for email and specialCharacter check

--- argus/LoginProviders/String+Extension.swift
+++ argus/LoginProviders/String+Extension.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+extension String {
+    func isValidEmail() -> Bool {
+        // here, `try!` will always succeed because the pattern is valid
+        let regex = try! NSRegularExpression(pattern: "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", options: .caseInsensitive)
+        return regex.firstMatch(in: self, options: [], range: NSRange(location: 0, length: characters.count)) != nil
+    }
+    
+    func containsSpecialCharacter() -> Bool {
+        let characterset = CharacterSet(charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+        if let _ = self.rangeOfCharacter(from: characterset.inverted) {
+            return true
+        }
+        return false
+    }
+}
