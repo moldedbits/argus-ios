5f761f52de9ec8f4cb40b157bc48a9073eafb085
Saurabh Gupta <saurabh@moldedbits.com>
2017-09-14 13:28:59 +0530

    resolves google sign in sdk/bridge error

--- argus/AppDelegate.swift
+++ argus/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import GoogleSignIn
+import Google
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,32 +17,47 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Initialize Google sign-in context
+        var configureError: NSError?
+        GGLContext.sharedInstance().configureWithError(&configureError)
+        assert(configureError == nil, "Error configuring Google services: \(configureError)")
+        
+        GIDSignIn.sharedInstance().delegate = self
+        
         return true
     }
+}
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+extension AppDelegate: GIDSignInDelegate {
+    
+    // MARK: Google SignIN delegate method
+    // TODO: Move this method in an extention to App Delegate
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if (error == nil) {
+            // Perform any operations on signed in user here.
+            
+            // TODO: Give a callback here to GoogleSignInProvider and set this user properties there only using init(user: User)
+            let userId = user.userID                  // For client-side use only!
+            let idToken = user.authentication.idToken // Safe to send to the server
+            let fullName = user.profile.name
+            let givenName = user.profile.givenName
+            let familyName = user.profile.familyName
+            let email = user.profile.email
+            // ...
+        } else {
+            // TODO:
+            print("\(error.localizedDescription)")
+        }
     }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    // MARK: Delegate method to handle user disconnection from the app
+    
+    func signIn(signIn: GIDSignIn!, didDisconnectWithUser user:GIDGoogleUser!,
+                withError error: NSError!) {
+        // Perform any operations when the user disconnects from app here.
+        // ...
     }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
 
--- argus/LoginProviders/GoogleLoginProvider.swift
+++ argus/LoginProviders/GoogleLoginProvider.swift
@@ -7,7 +7,53 @@
 //
 
 import Foundation
+import Google
+import GoogleSignIn
 
 class GoogleLoginProvider: BaseProvider {
     
 }
+
+// Method to be defined in ViewController
+
+//override func viewDidLoad() {
+//    super.viewDidLoad()
+//    
+//    GIDSignIn.sharedInstance().uiDelegate = self
+//    
+//    // Uncomment to automatically sign in the user.
+//    //GIDSignIn.sharedInstance().signInSilently()
+//    
+//    // TODO(developer) Configure the sign-in button look/feel
+//    // ...
+//}
+
+// Implement these methods only if the GIDSignInUIDelegate is not a subclass of
+// UIViewController.
+
+// Stop the UIActivityIndicatorView animation that was started when the user
+// pressed the Sign In button
+//func signInWillDispatch(signIn: GIDSignIn!, error: NSError!) {
+//    myActivityIndicator.stopAnimating()
+//}
+//
+
+//// Present a view that prompts the user to sign in with Google
+//func signIn(signIn: GIDSignIn!,
+//            presentViewController viewController: UIViewController!) {
+//    self.presentViewController(viewController, animated: true, completion: nil)
+//}
+//
+
+//// Dismiss the "Sign in with Google" view
+//func signIn(signIn: GIDSignIn!,
+//            dismissViewController viewController: UIViewController!) {
+//    self.dismissViewControllerAnimated(true, completion: nil)
+//}
+
+
+// Sign out the user
+
+//@IBAction func didTapSignOut(sender: AnyObject) {
+//    GIDSignIn.sharedInstance().signOut()
+//}
